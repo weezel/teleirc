8a9dc0fb96a18f38bacd0675ba7dbf069ec823ef
Ville Valkonen <weezelding@gmail.com>
2018-07-20 23:58:49 +0300

    Initial commit

---
+++ src/main/java/com/acme/MainApp.java
@@ -0,0 +1,16 @@
+package com.acme;
+
+import org.apache.camel.main.Main;
+
+
+public class MainApp
+{
+    public static void main(String[] args) throws Exception
+    {
+        Main main = new Main();
+        main.addRouteBuilder(new MyRouteBuilder());
+        main.run(args);
+    }
+
+}
+
---
+++ src/main/java/com/acme/MyRouteBuilder.java
@@ -0,0 +1,14 @@
+package com.acme;
+
+import org.apache.camel.builder.RouteBuilder;
+
+
+public class MyRouteBuilder extends RouteBuilder
+{
+
+    public void configure()
+    {
+
+    }
+
+}
