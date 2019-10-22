ae4922a4a19df2270e383e4977048c690142b7d0
Ville Valkonen <weezelding@gmail.com>
2019-10-22 20:59:04 +0300

    Reconnect when IRC server dies. Use constructor to get final constraint and easier testability. Start separating different message types.

--- src/main/java/com/acme/MainRoutes.java
+++ src/main/java/com/acme/MainRoutes.java
@@ -3,7 +3,11 @@ package com.acme;
 import com.acme.processors.IrcMessageProcessor;
 import com.acme.processors.TelegramMessageProcessor;
 import com.acme.telegramutils.TelegramUtils;
+import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.irc.IrcMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,29 +16,66 @@ import org.springframework.stereotype.Component;
 @Component
 public class MainRoutes extends RouteBuilder
 {
-    @Value("${irc.uri}")
-    private String ircUri;
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-    @Value("${telegram.uri}")
-    private String telegramUri;
+    private static final String FROM_IRC_ROUTE_ID = "from-irc";
+    private static final String FROM_TELEGRAM_ROUTE_ID = "from-telegram";
+
+    private final String ircUri;
+    private final String telegramUri;
+    private final TelegramUtils telegramUtils;
+    private final ChannelGroupMapper channelGroupMappings;
 
     @Autowired
-    private ChannelGroupMapper channelGroupMappings;
-
-    @Autowired
-    private TelegramUtils telegramUtils;
+    public MainRoutes(TelegramUtils telegramUtils,
+                      ChannelGroupMapper channelGroupMappings,
+                      @Value("${irc.uri}") String ircUri,
+                      @Value("${telegram.uri}") String telegramUri)
+    {
+        this.telegramUtils = telegramUtils;
+        this.channelGroupMappings = channelGroupMappings;
+        this.ircUri = ircUri;
+        this.telegramUri = telegramUri;
+    }
 
     @Override
     public void configure() throws Exception
     {
+        onException(RuntimeCamelException.class)
+            .process(exchange -> {
+                exchange.getOut().setBody(exchange.getIn().getBody());
+                exchange.getOut().setHeaders(exchange.getIn().getHeaders());
+
+                LOG.info("Stopping route {}", FROM_IRC_ROUTE_ID);
+                exchange.getContext().stopRoute(FROM_IRC_ROUTE_ID);
+                LOG.info("Starting route {}", FROM_IRC_ROUTE_ID);
+                exchange.getContext().startRoute(FROM_IRC_ROUTE_ID);
+            })
+            .delay(1000 * 60 * 2);
+
         from(ircUri)
-            .routeId("from-irc")
-            .process(new IrcMessageProcessor(channelGroupMappings))
-            .to(telegramUri)
-            .log("[IRC] -> Telegram delivered");
+            .routeId(FROM_IRC_ROUTE_ID)
+            .choice()
+                .when(header("irc.messageType").isEqualToIgnoreCase("PRIVMSG"))
+                    .process(new IrcMessageProcessor(channelGroupMappings))
+                    .to(telegramUri)
+                    .log("[IRC] -> Telegram delivered")
+                .otherwise()
+                .process(exchange -> {
+                    exchange.getOut().setBody(exchange.getIn().getBody());
+                    exchange.getOut().setHeaders(exchange.getIn().getHeaders());
+
+                    try {
+                        IrcMessage ircMsg = exchange.getOut().getBody(IrcMessage.class);
+                        LOG.info("{} {}", ircMsg.getMessageType(), ircMsg.getMessage());
+                    } catch (Exception e) {
+                        LOG.warn("Couldn't parse IRC message: {}", e.getMessage());
+                    }
+                })
+            .end();
 
         from(telegramUri)
-            .routeId("from-telegram")
+            .routeId(FROM_TELEGRAM_ROUTE_ID)
             .log("[Telegram] Incoming message")
             .process(new TelegramMessageProcessor(channelGroupMappings,
                                                   telegramUtils))
